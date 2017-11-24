ed10443e0b1fa686044583534d2866b31dc2d0a7
nikrb <nik_rb@yahoo.com>
2017-11-24 00:54:51 +0000

    wrap app in provider and router

--- src/index.js
+++ src/index.js
@@ -1,6 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {Provider} from "react-redux";
+import {ConnectedRouter} from "react-router-redux";
+import store, {history} from "./store";
+import App from "./containers/app";
 import './index.css';
-import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <Provider store={store}>
+    <ConnectedRouter history={history}>
+      <App />
+    </ConnectedRouter>
+  </Provider>,
+  document.getElementById('root')
+);
